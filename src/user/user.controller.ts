a2f332cfe95cd2a534c8c3a960107ec4ca57384f
mattrt13 <mehdiroozitalab13@gmail.com>
2024-10-20 21:50:32 +0330

    user services is ready

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDTO } from './user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('/api/user')
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+
+    @Get()
+    async getAll(): Promise<UserDTO[]> {
+        return this.userService.getAll();
+    }
+
+    @Post()
+    @HttpCode(201)
+    async createUser(@Body() userData: UserDTO): Promise<UserDTO> {
+        return this.userService.createUser(userData);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,67 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { UserDTO } from './user.dto';
+import { BookDTO } from 'src/book/book.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
+
+    async getAll(): Promise<UserDTO[]> {
+        const users = await this.userRepository.find();
+        return users.map(user => this.toDTO(user))
+    }
+
+    async createUser(userData: UserDTO): Promise<User> {
+        var available = await this.userRepository.find({
+            where: [
+                { username: userData.username, password: userData.password }
+            ]
+        });
+
+        if (available.length > 0) {
+            return available[0];
+        }
+
+        let result = this.userRepository.create({
+            username: userData.username,
+            password: userData.password,
+            email: userData.email
+        })
+
+        return this.userRepository.save(result);
+    }
+
+    async updateUser(id: number, newUserData: Partial<UserDTO>): Promise<User> {
+        const currentUser = await this.userRepository.findOneBy({ id });
+        if (!currentUser)
+            throw new Error("user not found");
+        const updatedUser = this.updateUserData(currentUser, newUserData);
+        return updatedUser;
+    }
+
+    async deleteUser(id: number): Promise<void> {
+        const currentUser = await this.userRepository.findOneBy({ id });
+        if (!currentUser)
+            throw new Error("user not found");
+        await this.userRepository.delete(currentUser);
+    }
+
+    private updateUserData(user: User, userData: Partial<UserDTO>): User {
+        user.username = userData.username;
+        user.password = userData.password;
+        user.email = userData.email;
+        return user;
+    }
+
+    private toDTO(user: User): UserDTO {
+        return {
+            username: user.username,
+            password: user.password,
+            email: user.email,
+        }
+    }
+}
